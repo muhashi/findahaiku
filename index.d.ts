5fc3b5afa831cebe7227d76e8521be5c34de138d
muhashi <105213357+muhashi@users.noreply.github.com>
2024-09-04 21:00:11 +1000

    Add prior version, add more detail in type summary

--- index.d.ts
+++ index.d.ts
@@ -15,6 +15,7 @@ Split up text into the lines of a haiku, if it is possible to split it up into l
 If a haiku cannot be made from the text, returns an empty list.
 
 @param input - Text to generate haiku from
+@param options - options to customize how the haiku is generated
 @returns A list of strings corresponding to the haiku line
 
 @example
@@ -23,6 +24,18 @@ import findahaiku from 'findahaiku';
 
 findahaiku('An old silent pond, a frog jumps into the pond! Splash; silence - again...');
 //=> ['An old silent pond,', 'a frog jumps into the pond!', 'Splash; silence - again...']
+
+findahaiku('Not a haiku!');
+//=> []
+
+findahaiku('This is much much much much much much much much much much much much much much much much much much much much too long to be a haiku.');
+//=> []
+
+findahaiku('This may be a real haiku, but it contains a word that is unknnnooooowwwwnnn!');
+//=> []
+
+findahaiku('This is much much much much much much much much much much much much much much much much much much much much too long to be a haiku.', { strict: false });
+//=> ['This is much much much', 'much much much much much much much', 'much much much much much']
 ```
 */
 export default function findahaiku(input: string, options?: Options): string[];
